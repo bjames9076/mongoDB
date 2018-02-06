4505dc7b23c11dcc94582baa8cf844446b990dba
Brandon James <bjames9076@gmail.com>
2018-02-06 18:07:36 -0500

    added comment mod

---
+++ models/comment.js
@@ -0,0 +1,19 @@
+// Require mongoose
+var mongoose = require("mongoose");
+// Create a schema class
+var Schema = mongoose.Schema;
+
+// Create the Note schema
+var NoteSchema = new Schema({
+  body: {
+    type: String
+  }
+});
+
+// These ids are referred to in the Article model
+
+// Create the model
+var comment = mongoose.model("Comment", CommentSchema);
+
+// Export the Note model
+module.exports = comment;
