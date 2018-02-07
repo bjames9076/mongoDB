ac6bcfca5b35b557145c8143d1fa12fb07668d8b
Brandon James <bjames9076@gmail.com>
2018-02-06 19:56:57 -0500

    connecting to monoglab

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ var routes = require("./controllers/controller.js");
 
 app.use("/", routes);
 
-mongoose.connect("mongodb://localhost/news-scraper");
+mongoose.connect("mongodb://local:root@ds125628.mlab.com:25628/heroku_tqvqbh0n");
 
 var db = mongoose.connection;
 
